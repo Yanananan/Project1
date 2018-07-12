65404775b72b44fc95f0c0c1ac2fb0f46f6074fb
LSLNG <lawrencesuilunng@gmail.com>
2018-07-11 21:04:50 -0700

    city list in JS

---
+++ app.js
@@ -0,0 +1,13 @@
+
+var destination = {
+    Barbados:{flightInfo: "flight 1", stufftodo: "asldkfjsl"},
+    Zurich: {flightInfo: "flight 1", stufftodo: "asldkfjsl"},
+    Havanna: {flightInfo: "flight 1", stufftodo: "asldkfjsl"},
+    Tokyo: {flightInfo: "flight 1", stufftodo: "asldkfjsl"},
+    Johannesburg: {flightInfo: "flight 1", stufftodo: "asldkfjsl"},
+    Manilla: {flightInfo: "flight 1", stufftodo: "asldkfjsl"},
+    Seoul: {flightInfo: "flight 1", stufftodo: "asldkfjsl"},
+    Sydney: {flightInfo: "flight 1", stufftodo: "asldkfjsl"},
+    Vancouver: {flightInfo: "flight 1", stufftodo: "asldkfjsl"},
+    Reykjavik: {flightInfo: "flight 1", stufftodo: "asldkfjsl"}
+}
