e8d5d12b08dc854421f02d738d42fb93529e5b9b
LSLNG <lawrencesuilunng@gmail.com>
2018-07-14 11:50:27 -0700

    redone JS should append info correctly

--- app.js
+++ app.js
@@ -14,130 +14,67 @@ var destination = {
 
 var weatherCount = 1;
 //weather API JS
-    // This is our API key. Add your own API key between the ""
-    var weatherAPIKey = "798ea114a6654cf1a775a5fce9773800";
-    var city = "";
-    var country = "";
-    var queryURL = "";
-    var newTable, newTr, newTd;
-    var queryURLResponse;
-    for(var obj in destination)
-    {
-        //debugger;
-        city = obj;
-        country = destination[city]["country"];  //FIX
-        //debugger;
-        //getWeather(city,country);
+// This is our API key. Add your own API key between the ""
+//var weatherAPIKey = "798ea114a6654cf1a775a5fce9773800";
 
+//2nd API key
+var weatherAPIKey = "d248772fc33441a58bfa6336cdc83ae6";
 
-        queryURL = "https://api.weatherbit.io/v2.0/forecast/daily?city="+city+"&country="+country+"&key="+weatherAPIKey;
-        queryURLResponse = $.ajax({url: queryURL});
-        console.log(queryURL);
-        //debugger;
-        
-       
-    
-    
-        // We then created an AJAX call
-        jQuery.when(queryURLResponse).done(function(response) {
-    
-          console.log(response);
-            
-          //create a table
-           
-            var content = "";
-          for(var i = 0; i < 7; i++)
-          {
-            content = "";
-          console.log(response.data[i].valid_date)
-        //   console.log(response.data[i].max_temp);
-        //   console.log(response.data[i].min_temp);
-        //   console.log(response.data[i].temp);
-        //   console.log(response.data[i].pop);
-        //   console.log(response.data[i].weather.description);
-        //   console.log(response.data[i].weather.icon);
-        //   console.log(response.data[i].weather.code);
-        //   console.log("https://www.weatherbit.io/static/img/icons/"+response.data[i].weather.icon+".png");
-          //debugger;
-            
-          // content += "<h6>Max Temp: "+ response.data[i].max_temp+"&#176; C</h6>";
-          // content += "<h6>Min Temp: "+ response.data[i].min_temp+"&#176; C</h6>";
-           content += "<h6>Avg Temp: "+ response.data[i].temp+"&#176; C</h6>";
-           content += "<h6>Precipitation: "+ response.data[i].pop +"%</h6>";
-           content += "<img src = 'https://www.weatherbit.io/static/img/icons/"+ response.data[i].weather.icon+".png' alt='icon' width='30' height='30'>";
-           content += "<br><h6>"+ response.data[i].weather.description +"</h6>";
-          $("#dest"+weatherCount+" .weather #day"+(i+1)).html(content);
-          console.log(weatherCount);
-        // debugger;
-        //   newTable = $("<table>");
-        //   newTr = $("<tr>");
-                
-               
-        //         newTd = $("<td>").html("weather content");
-        //         newTr.append(newTd);
-        //         newTable.append(newTr);
-        //         $("#dest"+weatherCount+" .weather").append(newTable);
-          //       weatherCount++;
-          }
-        
-          weatherCount++;
-        });
+//var city = "";
 
+var country = "";
 
+var queryURL = "";
+var newTable, newTr, newTd;
+var queryURLResponse;
+var resCount = 0;
 
-        //weatherCount++;
-    }
-    
+debugger;
 
-function getWeather(city,country){
-    // Here we are building the URL we need to query the database
-    queryURL = "https://api.weatherbit.io/v2.0/forecast/daily?city="+city+"&country="+country+"&key="+weatherAPIKey;
-    
-
+for(var city in destination)
+{
     //debugger;
     
-   
-
-
-    // We then created an AJAX call
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-
-      //console.log(response);
-        
-      //create a table
-       
-
-      for(var i = 0; i < 7; i++)
-      {
-      console.log(response.data[i].valid_date)
-      console.log(response.data[i].max_temp);
-      console.log(response.data[i].min_temp);
-      console.log(response.data[i].temp);
-      console.log(response.data[i].pop);
-      console.log(response.data[i].weather.description);
-      console.log(response.data[i].weather.icon);
-      console.log(response.data[i].weather.code);
-      console.log("https://www.weatherbit.io/static/img/icons/"+response.data[i].weather.icon+".png");
-      //debugger;
-
-      $("#dest"+weatherCount+" .weather #day"+(i+1)).html(response.data[i].valid_date);
-      debugger;
-    //   newTable = $("<table>");
-    //   newTr = $("<tr>");
-            
-           
-    //         newTd = $("<td>").html("weather content");
-    //         newTr.append(newTd);
-    //         newTable.append(newTr);
-    //         $("#dest"+weatherCount+" .weather").append(newTable);
-      //       weatherCount++;
-      }
+    country = destination[city]["country"];  //FIX
+ 
+    getWeather(city,country);
+    //debugger;
     
-      
-    });
+}
+
+function displayWeather()
+{
+    var content = "";
+    //debugger;
+    for(var city in destination)
+    {
+        for(var j = 0; j < 7; j++)
+        {
+            content = "";
+            content += "<h6>Avg Temp: "+ destination[city].weather.data[j].temp+"&#176; C</h6>";
+            content += "<h6>Precipitation: "+ destination[city].weather.data[j].pop +"%</h6>";
+            content += "<img src = 'https://www.weatherbit.io/static/img/icons/"+ destination[city].weather.data[j].weather.icon+".png' alt='icon' width='30' height='30'>";
+            content += "<br><h6>"+ destination[city].weather.data[j].weather.description +"</h6>";
+            $("#dest"+weatherCount+" .weather #day"+(j+1)).html(content);
+        }
+        weatherCount++;
+    }    
+}
+
+function getWeather(a,b)
+{
+    queryURL = "https://api.weatherbit.io/v2.0/forecast/daily?city="+a+"&country="+b+"&key="+weatherAPIKey;
+    console.log(queryURL);
+    $.ajax({url: queryURL}).then(function(response){
+        //var content = "";
+        //debugger;
+        destination[a].weather = response;
+        resCount++;
+        if(resCount == 10)
+        {
+            displayWeather();
+        }
+    })
 
 }
-    
+
