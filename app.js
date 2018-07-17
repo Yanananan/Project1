72339db0e453b9d42b5623e279f529379765bc57
Simon Nguyen <simonnguyen3054@gmail.com>
2018-07-17 16:54:24 -0700

    Add intercom

--- app.js
+++ app.js
@@ -1076,3 +1076,40 @@ $('.purchase').on('click', function(e) {
 window.addEventListener('popstate', function() {
   handler.close();
 });
+
+//intercom
+window.intercomSettings = {
+    app_id: "qcf7lbf5"
+};
+
+(function() {
+  var w = window;
+  var ic = w.Intercom;
+  if (typeof ic === 'function') {
+    ic('reattach_activator');
+    ic('update', intercomSettings);
+  } else {
+    var d = document;
+    var i = function() {
+      i.c(arguments);
+    };
+    i.q = [];
+    i.c = function(args) {
+      i.q.push(args);
+    };
+    w.Intercom = i;
+    function l() {
+      var s = d.createElement('script');
+      s.type = 'text/javascript';
+      s.async = true;
+      s.src = 'https://widget.intercom.io/widget/qcf7lbf5';
+      var x = d.getElementsByTagName('script')[0];
+      x.parentNode.insertBefore(s, x);
+    }
+    if (w.attachEvent) {
+      w.attachEvent('onload', l);
+    } else {
+      w.addEventListener('load', l, false);
+    }
+  }
+})();
